9478c5a851d77a8a8c1ffe786bdb2a215856ae20
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2017-07-29 12:53:15 +0200

    Sample data should have constant ID's

--- todo/sample_todos.go
+++ todo/sample_todos.go
@@ -3,8 +3,8 @@ package todo
 import "github.com/nicolaiskogheim/go-kit-graphql-todo/user"
 
 var (
-	Todo1 = &Todo{ID: NextTodoID(), Text: "Learn some GraphQL", Done: true, OwnerID: user.User1.ID}
-	Todo2 = &Todo{ID: NextTodoID(), Text: "Build an app", Done: false, OwnerID: user.User2.ID}
-	Todo3 = &Todo{ID: NextTodoID(), Text: "Finish that project", Done: true, OwnerID: user.User2.ID}
-	Todo4 = &Todo{ID: NextTodoID(), Text: "Eat breakfast", Done: true, OwnerID: user.User3.ID}
+	Todo1 = &Todo{ID: "7A421DFE", Text: "Learn some GraphQL", Done: true, OwnerID: user.User1.ID}
+	Todo2 = &Todo{ID: "3C2120A0", Text: "Build an app", Done: false, OwnerID: user.User2.ID}
+	Todo3 = &Todo{ID: "AF1BD873", Text: "Finish that project", Done: true, OwnerID: user.User2.ID}
+	Todo4 = &Todo{ID: "BAA13A54", Text: "Eat breakfast", Done: true, OwnerID: user.User3.ID}
 )
--- user/sample_users.go
+++ user/sample_users.go
@@ -1,8 +1,8 @@
 package user
 
 var (
-	User1 = &User{ID: NextUserID(), Name: "Jon", Email: "jon@jon.com", Password: "123"}
-	User2 = &User{ID: NextUserID(), Name: "Jim", Email: "jim@jim.com", Password: "123"}
-	User3 = &User{ID: NextUserID(), Name: "Jane", Email: "jane@jane.com", Password: "123"}
-	User4 = &User{ID: NextUserID(), Name: "Joane", Email: "joane@joane.com", Password: "123"}
+	User1 = &User{ID: "2C2E7C8D", Name: "Jon", Email: "jon@jon.com", Password: "123"}
+	User2 = &User{ID: "AC5CF9CF", Name: "Jim", Email: "jim@jim.com", Password: "123"}
+	User3 = &User{ID: "CCAE5161", Name: "Jane", Email: "jane@jane.com", Password: "123"}
+	User4 = &User{ID: "6E9BC35C", Name: "Joane", Email: "joane@joane.com", Password: "123"}
 )
